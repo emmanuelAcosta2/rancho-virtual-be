3e81566aa25389cb4cceb899094d736234de2b89
eacostacabana <emmanuel.acosta.cabana@gmail.com>
2023-08-14 23:52:07 +0000

    health check

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello Worl2!';
+    return 'Hello Worldddd!';
   }
   getHealth(): string {
     return 'Health check passed!';
